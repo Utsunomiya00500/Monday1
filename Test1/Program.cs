24b2dd228006c8de22a9b684fb46cb1caf0aafcf
AWang <Tom.901209@gmail.com>
2022-03-07 11:05:34 +0800

    加入專案檔案。

---
+++ Test1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ABC = "abc";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
