1e9afd612b7b0b48c21ab0f729da5ccec976038a
AWang <Tom.901209@gmail.com>
2022-03-07 11:51:12 +0800

    作業1

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -6,8 +6,12 @@ namespace Test1
     {
         static void Main(string[] args)
         {
-            string ABC = "abc123";
-            Console.WriteLine("Hello World!");
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            
+            int ALL = (int)meal_cost + tip_percent + tax_percent;
+            Console.WriteLine("你的餐費是" + ALL);
         }
     }
 }
